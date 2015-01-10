8fdfca9ab537a6adfc97c2507a6937746025e773
Rui Gil <rui.web@gmail.com>
2015-01-10 16:07:53 +0000

    Added functionalities related with bookmarking management.

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ var options = {
 	consumer_key: "",
 	access_token: "",
 	state: "all", // unread, archive, all
-	count: 10,
+	//count: 10,
 	target_folder: "pocketmark",
 	detail_type: "complete",
 	sort: "oldest" // newest, oldest, title, site
@@ -29,7 +29,25 @@ function makeRequest(method, url, data, callback) {
 	}
 }
 
-function retrieveBookmarks(callback) {
+function transformData(data) {
+	var e = data.list,
+		res = [];
+		console.log(e);
+	for(var x in e) {
+		var elm = e[x];
+		res.push({
+			order: elm["sort_id"],
+			url: elm["resolved_url"],
+			title: elm["resolved_title"],
+		});
+	}
+
+	return res.sort(function(a, b) {
+		return a.sort_id - b.sort_id;
+	});
+}
+
+function retrievePocketmarks(callback) {
 	var API_URI = "https://getpocket.com/v3/get";
 	makeRequest("POST", API_URI, {
 		consumer_key: options.consumer_key,
@@ -40,11 +58,76 @@ function retrieveBookmarks(callback) {
 		detail_type: "simple"
 	}, function(err, data) {
 		if(err) {
-			console.error(err);
+			callback(err);
 			return;
 		}
-		console.log("Data:");
-		console.log(data);
+		callback(null, transformData(data));
 	});
 };
 
+/**
+ * Gets a folder from it's name.
+ * If the search returns more than one result, the first is returned.
+ * Otherwise and if a <code>parent id</code> is provided, the folder
+ * is created.
+ *
+ * @param  {!string} folderName
+ * @param  {!function(string|null)} callback
+ * @param  {!string} parentId
+ */
+function getBookmarkFolder(folderName, callback, parentId) {
+	chrome.bookmarks.search({ title: folderName }, function(result) {
+		if(result.length >= 1) {
+			callback(result[0]);
+		} else if(result.length === 0) {
+			if(parentId) {
+				chrome.bookmarks.create({
+					'parentId': parentId,
+					'title': folderName,
+				}, function(f) {
+					callback(f);
+				});
+			}else {
+				callback(null);
+			}
+		}
+	});
+}
+
+function removeBookmark(bookmarkId, callback) {
+	chrome.bookmarks.remove(bookmarkId, callback?function() {
+			callback();
+		}:function(){});
+}
+
+function clearFolder(folderId, callback) {
+	chrome.bookmarks.getChildren(folderId, function(children) {
+		async.each(children, function(child, done) {
+			removeBookmark(child.id, done);
+		}, function(err) {
+			callback();
+		});
+	});
+}
+
+function addBookmark(parentId, title, url, callback) {
+	chrome.bookmarks.create({
+		parentId: parentId,
+		title: title,
+		url: url
+	}, callback ? function(bookmark) {
+			callback(bookmark);
+		} : function() {});
+}
+
+function update() {
+	retrievePocketmarks(function(err, pmarks) {
+		getBookmarkFolder(options.target_folder, function(folder) {
+			clearFolder(folder.id, function() {
+				async.each(pmarks, function(pmark, done) {
+					addBookmark(folder.id, pmark.title, pmark.url, function() { done() });
+				}, function(err) {});
+			});
+		}, "1"); // id:1 is bkmrk bar
+	});
+}
