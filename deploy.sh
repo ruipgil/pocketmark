9f53ef74666a3f28c84ce277099a3e5f3dfda978
Rui Gil <rui.web@gmail.com>
2015-02-16 23:47:59 +0000

    Minor fix in deply script.

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ evers=`git tag -l 'v'$version`
 if [ -z $evers ]
 then
 	rm ./src/consumer.key.js;
-	echo "var consumer_key=\"" $value "\";" > ./src/consumer.key.js;
-	zip -r pocketmark.v$version.zip bower_components libs/ src/ deploy.sh LICENSE manifest.json README.md;
+	echo "var consumer_key=\""$value"\";" > ./src/consumer.key.js;
+	zip -r pocketmark.v$version.zip bower_components src/ deploy.sh LICENSE manifest.json README.md .gitignore;
 	rm ./src/consumer.key.js;
 	echo "var consumer_key=\"{{your-consumer-key-here}}\";" > ./src/consumer.key.js;
 	git tag v$version;
