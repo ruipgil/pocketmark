d800f8a826d64a23f860e4f6234f0243f972a5d4
Rui Gil <ruipgil@gmail.com>
2017-01-07 17:38:52 +0000

    Clean build

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,13 @@
 #!/bin/sh
 value=`cat consumer.key`;
-version=`node --eval "console.log(require('./build/vendor/manifest.json').version)"`;
+version=`node --eval "console.log(require('./build/chrome/manifest.json').version)"`;
 evers=`git tag -l 'v'$version`
 if [ -z $evers ]
 then
-	rm ./js/consumer.key.js;
-	echo "var consumer_key=\""$value"\";" > ./src/consumer.key.js;
-	zip -r pocketmark.v$version.zip bower_components src/ deploy.sh LICENSE manifest.json README.md .gitignore resources/;
-	rm ./src/consumer.key.js;
-	echo "var consumer_key=\"{{your-consumer-key-here}}\";" > ./src/consumer.key.js;
+	echo "var consumer_key=\""$value"\";" > ./build/chrome/js/consumer.key.js;
+	zip -r pocketmark.v$version.zip build/chrome/;
 	git tag v$version;
 	echo 'Great success! If everything is good, push it';
-	.
 else
 	echo "There's a tag with this version (v"$version"), change the version on manifest.json"
-	.
 fi
--- js/consumer.key.js
+++
@@ -1,1 +0,0 @@
-var consumer_key="36623-21c65ca99095d25e1508a080";
